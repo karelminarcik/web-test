da79048240e658e581938b38c955d9633045a28d
Karel Minarcik <k.minarcik@seznam.cz>
2023-05-15 14:49:38 +0200

    test web page -index, css, grid

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="assets/grid.css">
+    <title>Firma</title>
+</head>
+<body>
+    <header>
+        <section class="nav-bar row">
+            <div id="logo">
+                <img src="img/logo.png" alt="firm">
+            </div>
+            <nav class="navigation">
+                <ul>
+                    <li><a href="index.php" class = "btn">HOME</a></li>
+                    <li><a href="kontakt.php" class = "btn">KONTAKT</a></li>
+                    <li><a href="fotogalerie.php" class = "btn">FOTOGALERIE</a></li>
+                    <li><a href="recenze.php" class = "btn">RECENZE</a></li>
+                </ul>
+            </nav>
+
+        </section>
+        
+    </header>
+
+    <main>
+        <section>
+            <br />
+            <h1>FIRM - Lorem ipsum dolor sit amet consectetur adipisici</h1>
+            <br />
+        </section>
+        <section class="our_projects heading-main">
+                <h2>Naše projekty</h2>
+        </section>
+        <section class="content">
+            
+            <div class="col span-1-of-2">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, velit tempora facilis iste ut accusamus necessitatibus! Reprehenderit, neque nostrum ipsam perspiciatis rem molestias, nisi vitae eaque illo magnam recusandae ab!</p>
+            </div>
+
+            <div class="col span-1-of-2">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, velit tempora facilis iste ut accusamus necessitatibus! Reprehenderit, neque nostrum ipsam perspiciatis rem molestias, nisi vitae eaque illo magnam recusandae ab!</p>
+            </div>
+
+        </section>
+       
+        <section class="photogalery row">
+            <div class="heading-main">
+                <h2>Fotogalerie</h2>
+            </div>
+            <div class="our_services">
+                <div class="col span-1-of-2">
+                    <img src="img/seo.jpg" alt="firm-seo">
+                </div>
+                <div class="col span-1-of-2">
+                    <img src="img/helpdesk.jpg" alt="firm-helpdesk">
+                </div>
+            </div>
+        </section>
+        <section class="referencies row">
+            <div class="heading-main">
+                <h2>Reference</h2>
+            </div>
+        </section>
+    </main>
+    
+    <footer>
+
+    </footer>
+</body>
+</html>
---
+++ recenze.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Recenze</title>
+</head>
+<body>
+    <header>
+        <section class="nav-bar row">
+            <div id="logo">
+                <img src="img/logo.png" alt="firm">
+            </div>
+            <nav class="navigation">
+                <ul>
+                    <li><a href="index.php" class = "btn">HOME</a></li>
+                    <li><a href="kontakt.php" class = "btn">KONTAKT</a></li>
+                    <li><a href="fotogalerie.php" class = "btn">FOTOGALERIE</a></li>
+                    <li><a href="recenze.php" class = "btn">RECENZE</a></li>
+                </ul>
+            </nav>
+
+        </section>
+        
+    </header>
+
+    <main>
+        <h1>RECENZE</h1>
+    </main>
+    
+    <footer>
+
+    </footer>
+</body>
+</html>
